5128eae62421d8e7ba649c25787e739ac1c0d8f0
Naved124 <nk7987513533@gmail.com>
2026-09-17 11:29:37 +0530

    extract job processing into worker.processJob

--- worker/cmd/worker/main.go
+++ worker/cmd/worker/main.go
@@ -16,6 +16,11 @@ import (
 	"github.com/Naved124/media-pipeline-worker/internal/transcode"
 )
 
+type worker struct {
+	QueueClient   *queue.Client
+	StorageClient *storage.Client
+}
+
 func main() {
 	// Our setup : create a SQS client
 	cfg, err := config.LoadDefaultConfig(context.Background())
@@ -35,6 +40,11 @@ func main() {
 
 	storageClient := storage.NewClient(s3Client, inputBucket, outputBucket)
 
+	w := &worker{
+		QueueClient:   queueClient,
+		StorageClient: storageClient,
+	}
+
 	for {
 		// loop forever:
 		//    receive one message from SQS -> get back: object key, receipt handle, found (yes/no), error
@@ -49,29 +59,38 @@ func main() {
 		if !found {
 			continue
 		}
-		jobID := uuid.New().String()
-		localPath, err := storageClient.Download(msg.ObjectKey)
-		if err != nil {
-			log.Printf("failed to download %s: %v", msg.ObjectKey, err)
+
+		if err := w.processJob(msg); err != nil {
+			log.Printf("job fialed : %v", err)
 			continue
-		}
-		log.Printf("path of the file: %s", localPath)
-
-		files, err := transcode.Transcode(localPath)
-		if err != nil {
-			log.Printf("files not fetched: %s, %v", jobID, err)
-			continue
-		}
-		for _, n := range files {
-			log.Printf("filepath is : %s , resolution is : %s", n.FilePath, n.Resolution)
-			key := fmt.Sprintf("%s/%s.mp4", jobID, n.Resolution)
-
-			err := storageClient.Upload(n.FilePath, key)
-			if err != nil {
-				log.Printf("this job failed : %s, %v", jobID, err)
-			}
-
 		}
 
 	}
+}
+
+func (w *worker) processJob(msg queue.Message) error {
+	jobID := uuid.New().String()
+	localPath, err := w.StorageClient.Download(msg.ObjectKey)
+	if err != nil {
+		return fmt.Errorf("failed to download %s: %w", msg.ObjectKey, err)
+	}
+	log.Printf("path of the file: %s", localPath)
+
+	files, err := transcode.Transcode(localPath)
+	if err != nil {
+		return fmt.Errorf("files not fetched: %s, %w", jobID, err)
+	}
+	for _, n := range files {
+		log.Printf("filepath is : %s , resolution is : %s", n.FilePath, n.Resolution)
+		key := fmt.Sprintf("%s/%s.mp4", jobID, n.Resolution)
+
+		err := w.StorageClient.Upload(n.FilePath, key)
+		if err != nil {
+			return fmt.Errorf("this job failed : %s, %s, %w", jobID, key, err)
+		}
+
+	}
+
+	return nil
+
 }
